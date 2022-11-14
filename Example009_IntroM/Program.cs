5d3fc9dee3be989b323cfd1cd05ba049102e6995
«Roman <research777@mail.ru>
2022-11-14 20:38:39 +0300

    Example009_IntroM

--- Example009_IntroM/Program.cs
+++ Example009_IntroM/Program.cs
@@ -77,7 +77,7 @@ int c3 = 313;
 //int max = Max(max1, max2, max3);
 
 int max = Max(                               //простая функция
-    Max(a1, 1, c1),
+    Max(a1, b1, c1),
     Max(a2, b2, c2),
     Max(a3, b3, c3));
 
