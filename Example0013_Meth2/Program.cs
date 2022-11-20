23297b1efd47d1d082525d0be494eb6ec730ad57
«Roman <research777@mail.ru>
2022-11-20 18:19:33 +0300

    Example0013_Meth2_v2

--- Example0013_Meth2/Program.cs
+++ Example0013_Meth2/Program.cs
@@ -16,3 +16,34 @@ Method1();
 
 //Вид 2
 
+/*
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+*/
+
+/*
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg:"Текст сообщения");
+*/
+/*
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Текст", 4);
+//Другие способы вызова метода:
+//Method21(msg: "Текст", count: 4);
+//Method21(count: 4, msg: "новый текст" );
+*/
+
